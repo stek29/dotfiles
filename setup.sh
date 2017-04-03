c4490d6436a3967142156bf09aab6aaa3c51bcbf
Viktor Oreshkin <stek29@users.noreply.github.com>
2017-04-03 05:55:26 +0300

    Remove unnecessary utils

--- setup.sh
+++ setup.sh
@@ -3,12 +3,6 @@
 # This is safe to run multiple times and will prompt you about anything unclear
 
 # Utils
-answer_is_yes() {
-  [[ "$REPLY" =~ ^[Yy]$ ]] \
-    && return 0 \
-    || return 1
-}
-
 ask_for_confirmation() {
   while true; do
     read "?$(print_question "$1")" yn
@@ -22,7 +16,6 @@ ask_for_confirmation() {
 }
 
 ask_for_sudo() {
-
   # Ask for the administrator password upfront
   sudo -v
 
@@ -33,53 +26,11 @@ ask_for_sudo() {
     sleep 60
     kill -0 "$$" || exit
   done &> /dev/null &
-
-}
-
-cmd_exists() {
-  [ -x "$(command -v "$1")" ] \
-    && printf 0 \
-    || printf 1
 }
 
 execute() {
   eval $1
   print_result $? "${2:-$1}"
-}
-
-get_os() {
-
-  declare -r OS_NAME="$(uname -s)"
-  local os=""
-
-  if [ "$OS_NAME" == "Darwin" ]; then
-    os="osx"
-  elif [ "$OS_NAME" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
-    os="ubuntu"
-  fi
-
-  printf "%s" "$os"
-
-}
-
-is_git_repository() {
-  [ "$(git rev-parse &>/dev/null; printf $?)" -eq 0 ] \
-    && return 0 \
-    || return 1
-}
-
-mkd() {
-  if [ -n "$1" ]; then
-    if [ -e "$1" ]; then
-      if [ ! -d "$1" ]; then
-        print_error "$1 - a file with the same name already exists!"
-      else
-        print_success "$1"
-      fi
-    else
-      execute "mkdir -p $1" "$1"
-    fi
-  fi
 }
 
 print_error() {
@@ -125,8 +76,8 @@ dir_backup=~/dotfiles_old             # old dotfiles backup directory
 
 # Get current dir (so run this script from anywhere)
 
-export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export DOTFILES_DIR
 
 # Create dotfiles_old in homedir
 echo -n "Creating $dir_backup for backup of any existing dotfiles in ~..."
